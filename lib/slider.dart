022d81934893f6b63296aba5ac64e1737d717847
IAM_VISHNU <vishnubs03011999@gmail.com>
2022-11-30 11:41:05 +0530

    volume_slider

---
+++ lib/slider.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  double _currentSliderValue = 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.volume_down_rounded),
+          Slider(
+            value: _currentSliderValue,
+            max: 100,
+            divisions: 50,
+            label: _currentSliderValue.round().toString(),
+            onChanged: (double value) {
+              setState(() {
+                _currentSliderValue = value;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
